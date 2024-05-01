ca45f740dd6f522f185d04b13a5e79291b243be9
FuryACE007 <sudhanshuvshekhar@gmail.com>
2024-05-01 12:17:52 +0530

    added wallet adapter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
 import Layout from "@/components/Layout";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useMemo } from "react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const rpcEndpoint = process.env.NEXT_PUBLIC_RPC_ENDPOINT;
+  const wallets = useMemo(() => [], []);
+
+  if (!rpcEndpoint) {
+    throw new Error("RPC endpoint is not defined. ");
+  }
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ConnectionProvider endpoint={rpcEndpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
--- src/useUmi.ts
+++ src/useUmi.ts
@@ -3,30 +3,29 @@
 import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 import type { Umi } from "@metaplex-foundation/umi";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { useWallet } from "@solana/wallet-adapter-react";
 import { useMemo } from "react";
 
 export function useUmi(): Umi {
-  //   const wallet = useWallet();
-  const rpcEndpoint = process.env.NEXT_PUBLIC_RPC_ENDPOINT;
+  const wallet = useWallet();
+  const rpcEndpoint = process.env.REACT_APP_RPC_ENDPOINT;
 
   if (!rpcEndpoint) {
     throw new Error("RPC endpoint is not defined. ");
   }
 
   const umi = useMemo(() => {
-    return (
-      createUmi(rpcEndpoint)
-        //   .use(walletAdapterIdentity(wallet))
-        .use(mplTokenMetadata())
-        .use(
-          irysUploader({
-            address: "https://devnet.irys.xyz",
-          })
-        )
-    );
-    //   }, [wallet]);
-  }, [rpcEndpoint]);
+    return createUmi(rpcEndpoint)
+      .use(walletAdapterIdentity(wallet))
+      .use(mplTokenMetadata())
+      .use(
+        irysUploader({
+          address: "https://devnet.irys.xyz",
+        })
+      );
+  }, [wallet]);
 
   if (!umi) {
     throw new Error(
