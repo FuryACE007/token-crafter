cdb2020d19515297ff560ece6f4a9cb47c0771c3
FuryACE007 <sudhanshuvshekhar@gmail.com>
2024-05-01 19:18:35 +0530

    token creation successful

--- src/utils/token-operations.ts
+++ src/utils/token-operations.ts
@@ -1,7 +1,54 @@
-import { Umi } from "@metaplex-foundation/umi";
+import { createFungible } from "@metaplex-foundation/mpl-token-metadata";
+import {
+  Umi,
+  createGenericFileFromBrowserFile,
+  generateSigner,
+  percentAmount,
+} from "@metaplex-foundation/umi";
+import toast from "react-hot-toast";
 
-export const createTokenService = async (umi: Umi, tokenName: String, tokenSymbol: String, tokenDescription: String, tokenImage: File) => {
+export const createTokenService = async (
+  umi: Umi,
+  tokenName: string,
+  tokenSymbol: string,
+  tokenDescription: string,
+  tokenImage: File
+) => {
+  const mint = generateSigner(umi);
+  const imageFile = await createGenericFileFromBrowserFile(tokenImage, {
+    contentType: "image/png",
+  });
+  const [fileUri] = await umi.uploader.upload([imageFile]);
+  toast.success("Image uploaded!");
+  const tokenMetadata = {
+    name: tokenName,
+    symbol: tokenSymbol,
+    description: tokenDescription,
+    image: fileUri,
+  };
+  const uri = await umi.uploader.uploadJson([tokenMetadata]);
+  toast.success("Metadata uploaded!");
 
+  createFungible(umi, {
+    mint,
+    name: tokenName,
+    symbol: tokenSymbol,
+    uri,
+    // payer: the gas sponsor wallet
+    sellerFeeBasisPoints: percentAmount(0),
+    isMutable: true,
+    isCollection: false,
+    authority: umi.identity, // the address which is allowed to mint the tokens
+    decimals: 3, // the divisibility of the fungible token
+  })
+    .sendAndConfirm(umi)
+    .then(() => {
+      console.log(
+        tokenMetadata.name + "created successfully: ",
+        mint.publicKey
+      );
+      toast.success("🦄 Token created successfully!");
+    });
 };
 export const checkSolBalance = async () => {};
 export const mintTokenHandler = async () => {};
