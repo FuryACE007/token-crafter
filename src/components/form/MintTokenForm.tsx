4b9121d17994c4d92e04af34ec2a69e4c3808a7f
FuryACE007 <sudhanshuvshekhar@gmail.com>
2024-06-27 20:37:38 +0530

    added token mint ui

--- src/components/form/MintTokenForm.tsx
+++ src/components/form/MintTokenForm.tsx
@@ -4,8 +4,13 @@ import toast, { Toaster } from "react-hot-toast";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import FileUpload from "./FileUpload";
+import { Umi } from "@metaplex-foundation/umi";
 
-const Form = () => {
+interface MintTokenFormProps {
+  umi: Umi;
+}
+
+const MintTokenForm: React.FC<MintTokenFormProps> = ({ umi }) => {
   const mintTokenHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     toast.success("Token Minted!", {
@@ -24,33 +29,17 @@ const Form = () => {
       <form className="my-8" onSubmit={mintTokenHandler}>
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
-            <Label htmlFor="tokenName">Token Name</Label>
+            <Label htmlFor="tokenMint">Token Mint</Label>
             <Input
-              id="tokenName"
+              id="tokenMint"
               placeholder="TokenCrafters"
               type="text"
               required
             />
           </LabelInputContainer>
           <LabelInputContainer>
-            <Label htmlFor="tokenSymbol">Token Symbol</Label>
-            <Input id="tokenSymbol" placeholder="TCX" type="text" required />
-          </LabelInputContainer>
-        </div>
-        <LabelInputContainer>
-          <Label htmlFor="tokenDescription">Token Description</Label>
-          <Input
-            id="tokenDescription"
-            placeholder="Some info about the token ( optional )"
-            type="text"
-          />
-        </LabelInputContainer>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 my-4">
-          <LabelInputContainer>
-            <Label className="mb-2" htmlFor="tokenImage">
-              Token Image
-            </Label>
-            <FileUpload />
+            <Label htmlFor="amount">Amount</Label>
+            <Input id="amount" placeholder="TCX" type="text" required />
           </LabelInputContainer>
         </div>
 
@@ -58,7 +47,7 @@ const Form = () => {
           className="mt-10 bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Craft 🛠️
+          Mint 💰
           <BottomGradient />
         </button>
       </form>
@@ -89,4 +78,4 @@ const LabelInputContainer = ({
   );
 };
 
-export default Form;
+export default MintTokenForm;
